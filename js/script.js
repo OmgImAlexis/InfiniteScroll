9c88f07e24004d780b87fb7c5e347aec15a79552
Alexis Tyler <OmgImAlexis@gmail.com>
2014-03-28 13:46:34 +1030

    Added on load, etc.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 (function(){
-  var entry_set = entry || '.entry';
-  var container_set = container || '#posts';
-  var url = window.location.pathname;
-  var page = url.split('/')[1];
-  var nextPage = String($('#nextPage').attr('href'));
-  $.get(nextPage, function(data){ 
-    $(data).find("#posts .entry").appendTo("#posts");
-    if (page == 'tagged'){
-      var tag = url.split('/')[2];
-      $('#nextPage').attr('href', "/tagged/" + tag + "/page/" + (parseInt($("#nextPage").attr("href").match(/\d+/))+1));
-    } else {
-      $('#nextPage').attr('href', "/page/" + (parseInt($("#nextPage").attr("href").match(/\d+/))+1));
-    }
-    var msnry = new Masonry( container, {
-      itemSelector: entry
+  $(window).on("load resize scroll",function(e){
+    var url = window.location.pathname;
+    var page = url.split('/')[1];
+    var nextPage = String($('#nextPage').attr('href'));
+    $.get(nextPage, function(data){ 
+      $(data).find("#posts .entry").appendTo("#posts");
+      if (page == 'tagged'){
+        var tag = url.split('/')[2];
+        $('#nextPage').attr('href', "/tagged/" + tag + "/page/" + (parseInt($("#nextPage").attr("href").match(/\d+/))+1));
+      } else {
+        $('#nextPage').attr('href', "/page/" + (parseInt($("#nextPage").attr("href").match(/\d+/))+1));
+      }
+      var msnry = new Masonry( container, {
+        itemSelector: entry
+      });
     });
   });
 })();
