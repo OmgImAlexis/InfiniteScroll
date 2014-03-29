a35bef5726e21cef9016fd66ea48dbb7be9c4fdf
Alexis Tyler <OmgImAlexis@gmail.com>
2014-03-29 21:45:42 +1030

    Fixed it loading on post pages.

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Add this to your theme before </ head> and then replace .entry with your entry c
 var entry = '.entry';
 </script>
 <script type="text/javascript" src="http://masonry.desandro.com/masonry.pkgd.min.js"></script>
-<script type="text/javascript" src="http://static.tumblr.com/ib6vvvn/L81n34rly/script.js"></script>
+<script type="text/javascript" src="http://static.tumblr.com/ib6vvvn/6NPn3738l/script.js"></script>
 ```
--- js/script.js
+++ js/script.js
@@ -3,28 +3,30 @@
     var url = window.location.pathname;
     var page = url.split('/')[1];
     var container = $(entry).parent();
-    if (page == 'tagged'){
-      var tag = url.split('/')[2];
-      if ($('#nextPage').length != 0){
-        $('#nextPage').attr('href', "/tagged/" + tag + "/page/" + (parseInt($("#nextPage").attr("href").match(/\d+/))+1));
+    if (page != 'post'){
+      if (page == 'tagged'){
+        var tag = url.split('/')[2];
+        if ($('#nextPage').length != 0){
+          $('#nextPage').attr('href', "/tagged/" + tag + "/page/" + (parseInt($("#nextPage").attr("href").match(/\d+/))+1));
+        } else {
+          $(container).parent().append('<div id="pagination"><a id="nextPage" href="tagged/' + tag  + '/page/2"></a></div>');
+        }
       } else {
-        $(container).parent().append('<div id="pagination"><a id="nextPage" href="tagged/' + tag  + '/page/2"></a></div>');
+        if ($('#nextPage').length != 0){
+          $('#nextPage').attr('href', "/page/" + (parseInt($("#nextPage").attr("href").match(/\d+/))+1));
+        } else {
+          $('#nextPage').attr('href', "/page/2");
+        }
       }
-    } else {
-      if ($('#nextPage').length != 0){
-        $('#nextPage').attr('href', "/page/" + (parseInt($("#nextPage").attr("href").match(/\d+/))+1));
-      } else {
-        $('#nextPage').attr('href', "/page/2");
-      }
-    }
-    var nextPage = String($('#nextPage').attr('href'));
-    $.get(nextPage, function(data){
-      $(data).find(entry).appendTo(container);
-      var msnry_container = document.querySelector('#' + container.attr("id"));
-      var msnry = new Masonry(msnry_container, {
-        itemSelector: entry
+      var nextPage = String($('#nextPage').attr('href'));
+      $.get(nextPage, function(data){
+        $(data).find(entry).appendTo(container);
+        var msnry_container = document.querySelector('#' + container.attr("id"));
+        var msnry = new Masonry(msnry_container, {
+          itemSelector: entry
+        });
       });
-    });
+    }
   }
   $(window).on("load resize",function(e){
     load();
