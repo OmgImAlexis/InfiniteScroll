e2283c274f2fe8b5922ee02843ed36579c45dc84
Alexis Tyler <OmgImAlexis@gmail.com>
2014-07-17 11:43:27 +0930

    Changed to new infinite scroll.

--- README.md
+++ README.md
@@ -1,11 +1,49 @@
 Tumblr Infinite Scroll
 ==============
 <br>
-Add this to your theme before </ head> and then replace .entry with your entry class/ID.
+Add this to your theme before </ head> and then replace .post with your post class/ID and #posts with your post wrapper class/ID.
 ```
-<script>
-var entry = '.entry';
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+<script type="text/javascript" src="http://isotope.metafizzy.co/isotope.pkgd.min.js"></script>
+<script type="text/javascript" src="http://static.tumblr.com/0xll37k/6QRn49wdh/infinitescroll.js"></script>
+<script type="text/javascript" src="http://imagesloaded.desandro.com/imagesloaded.pkgd.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    //http://www.shambix.com/en/isotope-twitter-bootstrap-infinite-scroll-fluid-responsive-layout/
+    var $container = $('#posts');
+    // Fire Isotope only when images are loaded
+    $container.imagesLoaded(function(){
+        $container.isotope({
+            itemSelector : '.post',
+            masonry: {
+            }
+        });
+    });
+
+    // Infinite Scroll
+    $('#posts').infinitescroll({
+        navSelector  : '#pagination',
+        nextSelector : '#pagination a',
+        itemSelector : '.post',
+        bufferPx     : 200,
+        loading: {
+            finishedMsg: 'We\'re done here.',
+        }
+    },
+
+    // Infinite Scroll Callback
+    function( newElements ) {
+        var $newElems = jQuery( newElements ).hide();
+        $newElems.imagesLoaded(function(){
+            $newElems.fadeIn();
+            $container.isotope( 'appended', $newElems );
+        });
+    });
+
+    if ($('.post:last').position().top > 200) {
+        $('#posts').infinitescroll('retrieve');
+    }
+});
 </script>
-<script type="text/javascript" src="http://masonry.desandro.com/masonry.pkgd.min.js"></script>
-<script type="text/javascript" src="http://static.tumblr.com/ib6vvvn/Tpjn3e8fv/script.js"></script>
+
 ```
--- js/script.js
+++ js/script.js
@@ -1,44 +1,42 @@
-(function(){
-  function load(){
-    var url = window.location.pathname;
-    var page = url.split('/')[1];
-    var container = $(entry).parent();
-    if (page != 'post'){
-      if (page == 'tagged'){
-        var tag = url.split('/')[2];
-        if ($('#nextPage').length != 0){
-          $('#nextPage').attr('href', "/tagged/" + tag + "/page/" + (parseInt($("#nextPage").attr("href").match(/\d+/))+1));
-        } else {
-          $(container).parent().append('<div id="pagination"><a id="nextPage" href="tagged/' + tag  + '/page/2"></a></div>');
-        }
-      } else {
-        if ($('#nextPage').length != 0){
-          $('#nextPage').attr('href', "/page/" + (parseInt($("#nextPage").attr("href").match(/\d+/))+1));
-        } else {
-          $('#nextPage').attr('href', "/page/2");
-        }
-      }
-      var nextPage = String($('#nextPage').attr('href'));
-      $.get(nextPage, function(data){
-        $(data).find(entry).appendTo(container);
-        var msnry_container = document.querySelector('#' + container.attr("id"));
-        var msnry = new Masonry(msnry_container, {
-          itemSelector: entry
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+<script type="text/javascript" src="http://isotope.metafizzy.co/isotope.pkgd.min.js"></script>
+<script type="text/javascript" src="http://static.tumblr.com/0xll37k/6QRn49wdh/infinitescroll.js"></script>
+<script type="text/javascript" src="http://imagesloaded.desandro.com/imagesloaded.pkgd.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    //http://www.shambix.com/en/isotope-twitter-bootstrap-infinite-scroll-fluid-responsive-layout/
+    var $container = $('#posts');
+    // Fire Isotope only when images are loaded
+    $container.imagesLoaded(function(){
+        $container.isotope({
+            itemSelector : '.post',
+            masonry: {
+            }
         });
-      });
-    } else {
-      var a = $('.more_notes_link')[0];
-      var e = document.createEvent('MouseEvents');
-      e.initEvent( 'click', true, true );
-      a.dispatchEvent(e);
+    });
+
+    // Infinite Scroll
+    $('#posts').infinitescroll({
+        navSelector  : '#pagination',
+        nextSelector : '#pagination a',
+        itemSelector : '.post',
+        bufferPx     : 200,
+        loading: {
+            finishedMsg: 'We\'re done here.',
+        }
+    },
+
+    // Infinite Scroll Callback
+    function( newElements ) {
+        var $newElems = jQuery( newElements ).hide();
+        $newElems.imagesLoaded(function(){
+            $newElems.fadeIn();
+            $container.isotope( 'appended', $newElems );
+        });
+    });
+
+    if ($('.post:last').position().top > 200) {
+        $('#posts').infinitescroll('retrieve');
     }
-  }
-  $(window).on("load resize",function(e){
-    load();
-  });
-  $(window).on("scroll",function(e){
-    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-      load();
-    }
-  });
-})();
+});
+</script>
